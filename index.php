47ac2c549c5f91ad294fb4ec4d770a4b723b02ed
Zetsuky <clarence16estacio@gmail.com>
2025-08-07 15:34:14 +0800

    created a navbar

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.7/css/bootstrap.min.css" integrity="sha512-fw7f+TcMjTb7bpbLJZlP8g2Y4XcCyFZW8uy8HsRZsH/SwbMw0plKHFHr99DN3l04VsYNwvzicUX/6qurvIxbxw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.7/js/bootstrap.min.js" integrity="sha512-zKeerWHHuP3ar7kX2WKBSENzb+GJytFSBL6HrR2nPSR1kOX1qjm+oHooQtbDpDBSITgyl7QXZApvDfDWvKjkUw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  
+  <title>CRE- Product System</title>
+</head>
+<body>
+  <nav class = "navbar navbar-expand-sm bg-dark navbar-dark">
+    <div class= "container-fluid">
+      <a href="#" class="navbar-brand">Logo</a>
+      <button class="navbar-toggler" type="button" 
+      data-bs-toggle="collapse" 
+      data-bs-target = #collapsibleNavbar>
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul class="navbar-nav"> 
+          <li class="nav-link" href="#"> Products</li>
+          <li class="nav-link" href="#"> Login</li>
+          <li class="nav-link" href="#"> Logout</li>
+      </ul>
+      </div>
+    </div>
+  </nav>
+</body>
+</html>
